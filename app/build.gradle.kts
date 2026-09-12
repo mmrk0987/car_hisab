24bd50c7248f86a2a45914b99b2ff1dafd65000e
mmrk0987 <147331993+mmrk0987@users.noreply.github.com>
2026-09-13 00:56:02 +0600

    Add Supabase and Ktor dependencies to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,9 @@ android {
   dependenciesInfo {
     includeInApk = false
     includeInBundle = true
+implementation("io.github.jan-tennert.supabase:postgrest-kt:3.0.0")
+implementation("io.github.jan-tennert.supabase:auth-kt:3.0.0")
+implementation("io.ktor:ktor-client-android:3.0.0")
   }
 }
 
